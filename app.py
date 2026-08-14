52a73afb59ac127645fcb36ca23bf3c64aeb1e96
howei09182004 <howei0918@gmail.com>
2026-08-14 10:50:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -767,4 +767,5 @@ def merge():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5002)
+    port = int(os.environ.get("PORT", 5002))
+    app.run(debug=False, host="0.0.0.0", port=port)
