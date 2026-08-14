758e166864c91fe333dc0b42cb813899f693e9db
howei09182004 <howei0918@gmail.com>
2026-08-14 12:09:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -766,6 +766,11 @@ def merge():
     return jsonify(result)
 
 
+@app.route("/membership")
+def membership():
+    return render_template("membership.html")
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5002))
     app.run(debug=False, host="0.0.0.0", port=port)
